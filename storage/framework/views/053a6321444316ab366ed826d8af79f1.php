1b4e0b32b8e6ea53cc025f9050da34b27a7a9e71
triawaneric <triawaneric@gmail.com>
2025-01-12 18:24:24 +0700

    Add storage folder to the repository

---
+++ storage/framework/views/053a6321444316ab366ed826d8af79f1.php
@@ -0,0 +1,14 @@
+<!-- resources/views/livewire/jumbotron.blade.php -->
+
+<div class="relative min-h-screen flex flex-col justify-center text-center bg-cover bg-center" style="background-image: url('<?php echo e(asset('storage/images/jumbotron/jumbotron.png')); ?>');">
+    <!-- Overlay -->
+    <div class="absolute inset-0 bg-black bg-opacity-15"></div>
+
+    <!-- Content -->
+    <div class="relative z-10 container mx-auto px-4 text-white">
+        <h1 class="text-4xl md:text-5xl font-extrabold mb-4">Discover Beautiful Jewelry</h1>
+        <p class="text-lg md:text-xl mb-6">Find the perfect piece to express your unique style and elegance.</p>
+        <a href="/products" class="inline-block bg-white text-gray-600 py-3 px-6 rounded-lg text-lg font-semibold hover:bg-gray-100 transition duration-300">Shop Now</a>
+
+    </div>
+</div><?php /**PATH /Users/triawaneric/Work/DIgiKreatif/atticpiercing/resources/views/livewire/jumbotron.blade.php ENDPATH**/ ?>
